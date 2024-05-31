29b3df98063613c9bacf7e28db877a51a514c4ee
Juan Melo <jmelopdev@gmail.com>
2024-06-01 00:09:45 +0100

    feat: 1929. Concatenation of Array

---
+++ LeetCode/concatenationArray.ts
@@ -0,0 +1,12 @@
+/* 1929. Concatenation of Array
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans. */
+
+function getConcatenation(nums: number[]): number[] {
+  return [...nums, ...nums];;
+};
+
+getConcatenation([1, 2, 1]);
