aa4ba0c5bf696e8e779e00a2af03ffdfb954f4be
Juan Melo <jmelopdev@gmail.com>
2024-06-13 19:53:24 +0100

    feat: 1. Two Sum - code formatted with prettier

--- LeetCode/twoSum.ts
+++ LeetCode/twoSum.ts
@@ -1,31 +1,22 @@
-/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/* 1. Two Sum. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order. */
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 function twoSum(nums: number[], target: number): number[] | null {
-  let leftPointer = 0;
-  let rightPointer = nums.length - 1;
+  let map = new Map();
 
-  while (leftPointer < rightPointer) {
-    let sum = nums[leftPointer] + nums[rightPointer];
-    if (sum === target) {
-      return [leftPointer, rightPointer];
-    } else if (leftPointer >= rightPointer - 1) {
-      rightPointer = nums.length - 1;
-      leftPointer = ++leftPointer;
-    } else {
-      rightPointer = rightPointer - 1;
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement) && map.get(complement) !== i) {
+      return [map.get(complement), i];
     }
+
+    map.set(nums[i], i);
   }
 
   return null;
-};
+}
 
-console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([2, 3, 2, 4], 6));
