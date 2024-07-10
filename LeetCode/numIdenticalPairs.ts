3644ec5a376c6191a3320d5bf5e02b7a64d02c98
Juan Melo <jmelopdev@gmail.com>
2024-07-10 23:20:29 +0100

    feat: 1512. Number of Good Pairs - Refactor

--- LeetCode/numIdenticalPairs.ts
+++ LeetCode/numIdenticalPairs.ts
@@ -5,24 +5,14 @@ A pair (i, j) is called good if nums[i] == nums[j] and i < j.
  */
 
 function numIdenticalPairs(nums: number[]): number {
-    let map1 = new Map();
-
-  nums.forEach((element, index) => {
-    if (!map1[element]) {
-      map1[element] = [];
-    }
-    map1[element].push(index);
-  });
-
+  let map1 = new Map();
   let counter = 0;
-  for (const key in map1) {
-    let keyLength = map1[key].length;
-    if (keyLength > 1) {
-        counter += (keyLength * (keyLength - 1)) / 2
-    }
+  for (var i = 0; i < nums.length; i++) {
+    counter += map1.get(nums[i]) ?? 0;
+    map1.set(nums[i], map1.get(nums[i]) + 1 || 1);
   }
 
   return counter;
-};
+}
 
 console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
