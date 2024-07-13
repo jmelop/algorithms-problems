31c2fe17cec36c84dc47e1a9bd2eab53684bff70
Juan Melo <jmelopdev@gmail.com>
2024-07-13 21:50:07 +0100

    feat: 27. Remove Element

---
+++ LeetCode/removeElement.ts
@@ -0,0 +1,20 @@
+/* 27. Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k. */
+
+function removeElement(nums: number[], val: number): number {
+  let k = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3));
