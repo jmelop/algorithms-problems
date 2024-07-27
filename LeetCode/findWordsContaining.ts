a7152453503e74794a02dc216fe6aaf1238c3493
Juan Melo <jmelopdev@gmail.com>
2024-07-27 22:52:51 +0100

    feat: 2942. Find Words Containing Character - Different approach using a while and a for loop with same efficiency

--- LeetCode/findWordsContaining.ts
+++ LeetCode/findWordsContaining.ts
@@ -6,6 +6,23 @@ Return an array of indices representing the words that contain the character x.
 Note that the returned array may be in any order. */
 
 function findWordsContaining(words: string[], x: string): number[] {
+  let array: number[] = [];
+  let i = 0;
+  while (i < words.length) {
+    for (let z = 0; z < words[i].length; z++) {
+      if (words[i][z] === x) {
+        array.push(i);
+        break;
+      }
+    }
+    i++;
+  }
+  return array;
+}
+
+console.log(findWordsContaining(["abc", "bcd", "aaaa", "cbc"], "a"));
+
+/* function findWordsContaining(words: string[], x: string): number[] {
     let array: number[] = [];
     for (var i = 0; i < words.length; i++) {
         if (words[i].includes(x)) {
@@ -13,6 +30,4 @@ function findWordsContaining(words: string[], x: string): number[] {
         }
     }
     return array;
-};
-
-console.log(findWordsContaining(["abc","bcd","aaaa","cbc"], "a"));
+}; */
