893732f4ec597ac03316cf9240df38b0192d1d18
Juan Melo <jmelopdev@gmail.com>
2024-08-05 00:24:07 +0100

    feat: 2235. Add Two Integers

---
+++ LeetCode/sum.ts
@@ -0,0 +1,8 @@
+/* 2235. Add Two Integers
+Given two integers num1 and num2, return the sum of the two integers. */
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log(sum(-10, 4));
