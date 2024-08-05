0647488fb092345c35c8cef7f4ab84ee196bed9c
Juan Melo <jmelopdev@gmail.com>
2024-08-05 23:15:33 +0100

    feat: 20. Valid Parentheses - First approach

---
+++ LeetCode/isValid.ts
@@ -0,0 +1,33 @@
+/* 20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type. */
+
+var isValid = function (s) {
+  const stack: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case "{":
+        stack.push("}");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      case "(":
+        stack.push(")");
+        break;
+      default:
+        if (s[i] !== stack.pop()) {
+          return false;
+        }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("{}[][]"));
